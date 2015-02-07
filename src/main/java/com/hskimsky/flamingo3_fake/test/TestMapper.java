4bac83e2693a7d8230b51ff1f344d406dc850b50
Kimsky <hskimsky@naver.com>
2015-02-07 13:51:53 +0900

    Mapper 추가

---
+++ src/main/java/com/hskimsky/flamingo3_fake/test/TestMapper.java
@@ -0,0 +1,14 @@
+package com.hskimsky.flamingo3_fake.test;
+
+import com.hskimsky.flamingo3_fake.annotation.Mapper;
+
+/**
+ * Description.
+ *
+ * @author Ha Neul Kim
+ * @version 1.0
+ * @since 2015-02-07
+ */
+@Mapper
+public interface TestMapper {
+}
